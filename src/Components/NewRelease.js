a42803f2410c855d070873b2cba798342b182769
zohaib <zohiab.ahmad@khaleef.com>
2019-12-03 13:37:42 +0500

    load more and place holder added in newRelease

--- src/Components/NewRelease.js
+++ src/Components/NewRelease.js
@@ -4,19 +4,21 @@ import Loader from './Loader';
 //********** Constants  **********//
 
 import { KCMS_URL, KCMS_SECRET_KEY, KCMS_PROJECT_ID } from '../../src/Constants';
+import i18n from '../Locales';
 
 
+const PER_PAGE = 15;
 
-const videoSongsFragment = (newCatListData, viewType) => {
+const videoSongsFragment = (data, viewType) => {
     return (
         <Fragment>
             <Link to={{
                 pathname: "/play",
-                state: 'content' in newCatListData[0] ? newCatListData[0].content[0] : newCatListData[0]
+                state: 'content' in data[0] ? data[0].content[0] : data[0]
             }}>
                 <div className="new-rel-container">
                     <img
-                        src={'content' in newCatListData[0] ? newCatListData[0].content[0].thumbs.original : newCatListData[0].thumbs.original}
+                        src={'content' in data[0] ? data[0].content[0].thumbs.original : data[0].thumbs.original}
                         alt=''
                     />
                     <span> <img src="icon-play.svg" alt='' /> </span>
@@ -28,17 +30,17 @@ const videoSongsFragment = (newCatListData, viewType) => {
             <div className="container mt-4">
                 <div className="row">
                     {
-                        newCatListData.map((item, index) => {
+                        data.map((item, index) => {
                             let imgSrc = ('content' in item ? item.content[0].custom_thumbnails.original : item.custom_thumbnails.original);
                             if (index > 0) {
                                 return (
 
                                     <div className="img-container pl-1 pr-1 vsongs col-4 col-sm-3 col-md-2" key={item.id}>
-                                        <Link to={{ pathname: "/play", state: ('content' in item)  ? item.content[0] : item }}>
+                                        <Link to={{ pathname: "/play", state: ('content' in item) ? item.content[0] : item }}>
                                             <img
                                                 className="imgThumb imgThumbSqr"
-                                                src={imgSrc}
-                                                
+                                                src={imgSrc || './placeholder.jpg'}
+
                                             />
                                             <div className="icon-play-songs"></div>
                                         </Link>
@@ -58,16 +60,16 @@ const videoSongsFragment = (newCatListData, viewType) => {
 }
 
 
-const moviesFragment = (newCatListData, viewType) => {
+const moviesFragment = (data, viewType) => {
     return (
         <Fragment>
             <Link to={{
                 pathname: "/play",
-                state: newCatListData[0].content ? newCatListData[0].content[0] : newCatListData[0]
+                state: data[0].content ? data[0].content[0] : data[0]
             }}>
                 <div className="new-rel-container">
                     <img
-                        src={newCatListData[0].content ? newCatListData[0].thumbnails[0].mobile_n_movie : newCatListData[0].custom_thumbnails.original} alt='' />
+                        src={data[0].content ? data[0].thumbnails[0].mobile_n_movie : data[0].custom_thumbnails.original} alt='' />
                     <span> <img src="icon-play.svg" alt='' /> </span>
                     <figcaption className="figcaption-4bnr-incremnt">
                         <h1 className="">&nbsp;</h1>
@@ -77,7 +79,7 @@ const moviesFragment = (newCatListData, viewType) => {
             <div className="container mt-4">
                 <div className="row">
                     {
-                        newCatListData.map((item, index) => {
+                        data.map((item, index) => {
                             let imgSrc = (item.content ? item.thumbnails[0].mobile_n_movie : item.custom_thumbnails.original);
                             if (index > 0) {
                                 return (
@@ -86,7 +88,7 @@ const moviesFragment = (newCatListData, viewType) => {
                                             pathname: "/play",
                                             state: item.content ? item.content[0] : item
                                         }}>
-                                            <img alt="" className="" src={imgSrc} />
+                                            <img alt="" className="" src={imgSrc || './placeholder.jpg'} />
                                         </Link>
                                     </div>
                                 );
@@ -105,16 +107,16 @@ const moviesFragment = (newCatListData, viewType) => {
 }
 
 
-const anyOtherTypeFragment = (newCatListData, viewType) => {
+const anyOtherTypeFragment = (data, viewType) => {
     return (
         <Fragment>
             <Link to={{
                 pathname: "/play",
-                state: newCatListData[0].content ? newCatListData[0].content[0] : newCatListData[0]
+                state: data[0].content ? data[0].content[0] : data[0]
             }}>
                 <div className="new-rel-container">
                     <img
-                        src={newCatListData[0].content ? newCatListData[0].content[0].thumbs.original : newCatListData[0].thumbs.original} alt='' />
+                        src={data[0].content ? data[0].content[0].thumbs.original : data[0].thumbs.original} alt='' />
                     <span> <img src="icon-play.svg" alt='' /> </span>
                     <figcaption className="figcaption-4bnr-incremnt">
                         <h1 className="">&nbsp;</h1>
@@ -124,7 +126,7 @@ const anyOtherTypeFragment = (newCatListData, viewType) => {
             <div className="container mt-4">
                 <div className="row">
                     {
-                        newCatListData.map((item, index) => {
+                        data.map((item, index) => {
                             let imgSrc = (item.content ? item.content[0].thumbs.original : item.thumbs.original);
                             if (index > 0) {
                                 return (
@@ -153,19 +155,27 @@ const anyOtherTypeFragment = (newCatListData, viewType) => {
 
 const NewRelease = (props) => {
 
-    const [newCatListData, setnewCatListData] = useState([]);
+    const [data, setData] = useState([]);
     const [viewType, setviewType] = useState("");
+    const [loading, setloading] = useState(true)
+    const [loadMore, setLoadMore] = useState(false)
+    const [currPage, setCurrPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(1)
+
 
     useEffect(() => {
 
         const getNewCatListData = async (data) => {
             try {
-                setnewCatListData([])
-                const url = `${KCMS_URL}/api/project/${KCMS_SECRET_KEY}/${KCMS_PROJECT_ID}/get-list-view-item-data/${props.location.state}`
+                setData([])
+                const url = `${KCMS_URL}/api/project/${KCMS_SECRET_KEY}/${KCMS_PROJECT_ID}/get-list-view-item-data/${props.location.state}?page=1&per_page=10`
                 const resp = await fetch(url);
                 const respObj = await resp.json();
+                console.log('totalPages', respObj)
                 setviewType(respObj.data.view_list_item_data.view_type)
-                setnewCatListData(respObj.data.view_list_item_data.data);
+                setData(respObj.data.view_list_item_data.data);
+                if (respObj.data.view_list_item_data.data.length < 10) setTotalPages(currPage)
+                else setTotalPages(currPage + 1)
             } catch (e) {
                 console.log("Error in new category  list data", e.message)
             }
@@ -174,16 +184,40 @@ const NewRelease = (props) => {
 
     }, [props.location.state, props.loaded])
 
+    const getMoreResult = async () => {
+        
+        setLoadMore(true)
+
+        let nextPage = currPage + 1;
+
+        const url = `${KCMS_URL}/api/project/${KCMS_SECRET_KEY}/${KCMS_PROJECT_ID}/get-list-view-item-data/${props.location.state}?page=${nextPage}&per_page=10`
+        let resp = await fetch(url)
+        let respObj = await resp.json()
+        console.log('more results', respObj)
+        setCurrPage(nextPage);
+        setLoadMore(false)
+        
+        
+        respObj.data.view_list_item_data.data.map((item) => {
+            setData(data => [...data, item]);
+        })
+        
+        if (respObj.data.view_list_item_data.data.length < 10) setTotalPages(currPage)
+        else setTotalPages(nextPage + 1)
+        console.log(currPage, totalPages)
+    }
+
     return (
-        newCatListData.length > 0 ? <Fragment>
-            <div style={{backgroundColor:'#1c1d26', marginBottom:'-3rem', paddingBottom:'3rem'}}>
-                {viewType.indexOf("portrait") >= 0 && moviesFragment(newCatListData, viewType)}
-                {viewType.indexOf("square") >= 0 && videoSongsFragment(newCatListData, viewType)}
-                {viewType.indexOf("landscape") >= 0 && anyOtherTypeFragment(newCatListData, viewType)}
+        data.length > 0 ? <Fragment>
+            <div style={{ backgroundColor: '#1c1d26', marginBottom: '-3rem', paddingBottom: '3rem' }}>
+                {viewType.indexOf("portrait") >= 0 && moviesFragment(data, viewType)}
+                {viewType.indexOf("square") >= 0 && videoSongsFragment(data, viewType)}
+                {viewType.indexOf("landscape") >= 0 && anyOtherTypeFragment(data, viewType)}
+                {((currPage < totalPages)) && <button disabled={loadMore} className="btn btn-primary btn-loadmore mr-auto ml-auto mb-5 mt-5" onClick={getMoreResult}> {loadMore ? i18n.loading : i18n.loadmore} </button>}
             </div>
         </Fragment>
-        :
-        <Loader />
+            :
+            <Loader />
 
     );
 
